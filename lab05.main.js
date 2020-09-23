19ee70e2ff723f0c5510e70549a2eb1af7d3a517
ab05165 <marcel.stalling1@centurylink.com>
2020-09-23 23:24:50 +0000

    Lab 6

--- lab05.main.js
+++ lab05.main.js
@@ -84,69 +84,20 @@ class ServiceNowAdapter extends EventEmitter {
   }
 
   /**
- * @memberof ServiceNowAdapter
- * @method healthcheck
- * @summary Check ServiceNow Health
- * @description Verifies external system is available and healthy.
- *   Calls method emitOnline if external system is available.
- *
- * @param {ServiceNowAdapter~requestCallback} [callback] - The optional callback
- *   that handles the response.
- */
- healthcheck(callback) {
-   this.getRecord((result, error) => {
-   /**
-    * For this lab, complete the if else conditional
-    * statements that check if an error exists
-    * or the instance was hibernating. You must write
-    * the blocks for each branch.
-    */
-    if (error) {
-     /**
-      * Write this block.
-      * If an error was returned, we need to emit OFFLINE.
-      * Log the returned error using IAP's global log object
-      * at an error severity. In the log message, record
-      * this.id so an administrator will know which ServiceNow
-      * adapter instance wrote the log message in case more
-      * than one instance is configured.
-      * If an optional IAP callback function was passed to
-      * healthcheck(), execute it passing the error seen as an argument
-      * for the callback's errorMessage parameter.
-      */
-    
-        this.emitOffline();
-        console.error(`\nError returned:\n${JSON.stringify(error)} for service instance ` + this.id );
-        
-        if ( callback !== null && ( callback.errorMessage !== null || callback.errorMessage ) ){
-            console.error(`\nHealthCheck :\n${JSON.stringify(callback.errorMessage)} for service instance ` + this.id );
-        }
-    } else if (result !== null && result.body.includes('Instance Hibernating page')) {
-
-        error = 'Service Now instance is hibernating';
-        this.emitOffline();
-        console.error(`\nError returned:\n${JSON.stringify(error)} for service instance ` + this.id );
-
-    }else {
-     /**
-      * Write this block.
-      * If no runtime problems were detected, emit ONLINE.
-      * Log an appropriate message using IAP's global log object
-      * at a debug severity.
-      * If an optional IAP callback function was passed to
-      * healthcheck(), execute it passing this function's result
-      * parameter as an argument for the callback function's
-      * responseData parameter.
-      */
-      this.emitOnline();
-        console.debug(`\nError returned:\n${JSON.stringify(result)} for service instance ` + this.id );
-        
-        if ( callback !== null && ( callback.responseData !== null || callback.responseData ) ){
-            console.error(`\nHealthCheck :\n${JSON.stringify(callback.responseData)} for service instance ` + this.id );
-        }
-    }
-  });
- }
+   * @memberof ServiceNowAdapter
+   * @method healthcheck
+   * @summary Check ServiceNow Health
+   * @description Verifies external system is available and healthy.
+   *   Calls method emitOnline if external system is available.
+   *
+   * @param {ServiceNowAdapter~requestCallback} [callback] - The optional callback
+   *   that handles the response.
+   */
+  healthcheck(callback) {
+    // We will build this method in a later lab. For now, it will emulate
+    // a healthy integration by emmitting ONLINE.
+    this.emitOnline();
+  }
 
   /**
    * @memberof ServiceNowAdapter
@@ -201,7 +152,7 @@ class ServiceNowAdapter extends EventEmitter {
      * Note how the object was instantiated in the constructor().
      * get() takes a callback function.
      */
-    return  this.connector.get( callback );
+    return  connector.get( callback );
   }
 
   /**
@@ -220,7 +171,7 @@ class ServiceNowAdapter extends EventEmitter {
      * Note how the object was instantiated in the constructor().
      * post() takes a callback function.
      */
-    return  this.connector.post(connector.options, callback );
+    return  connector.post(connector.options, callback );
   } 
   
 }
